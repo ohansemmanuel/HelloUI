85fbe5062dd54c7b02f4674a5085ddb353b57021
ohansemmanuel <ohansemmanuel@outlook.com>
2020-07-06 19:34:39 +0200

    initialise react-redux solution

---
+++ solutions/react-redux/src/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import logo from "./logo.png";
+import "./App.css";
+
+const PLANETS = ["Web", "Javascript", "Earth"];
+
+function App() {
+  const [activePlanet, setActivePlanet] = useState(PLANETS[0]);
+  const handlePlanetUpdate = (event) => {
+    setActivePlanet(event.target.textContent);
+  };
+
+  return (
+    <>
+      <img src={logo} alt="hello-world-logo" width="220" height="190" />
+      <p>{`Hello World from planet ${activePlanet}`}</p>
+
+      <section>
+        {PLANETS.map((planet) => (
+          <button
+            className={`btn ${planet === activePlanet ? "selected" : ""}`}
+            onClick={handlePlanetUpdate}
+            key={planet}
+          >
+            {planet}
+          </button>
+        ))}
+      </section>
+    </>
+  );
+}
+
+export default App;
